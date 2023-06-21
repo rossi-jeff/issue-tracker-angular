fb1c4b20747e7668bbd686fe92fe506143ed09d3
Jeff Rossi <rossi.jeff@gmail.com>
2023-06-21 17:05:10 -0400

    dashboard card

---
+++ src/app/dashboard-card/dashboard-card.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IssueType } from '../../types/issue.type';
+import { getFullName } from '../../lib/get-full-name';
+
+@Component({
+  selector: 'app-dashboard-card',
+  templateUrl: './dashboard-card.component.html',
+  styleUrls: ['./dashboard-card.component.css'],
+})
+export class DashboardCardComponent implements OnInit {
+  @Input() issue!: IssueType;
+
+  project: string = '';
+  author: string = '';
+  assignedTo: string = '';
+
+  toggle = (id: string) => {
+    const el = document.getElementById(id);
+    if (el) el.classList.toggle('open');
+  };
+
+  toggleContent = () => {
+    this.toggle(`dashboard-card-content-${this.issue.Id}`);
+  };
+
+  toggleDescription = () => {
+    this.toggle(`dashboard-card-description-${this.issue.Id}`);
+  };
+
+  toggleDetails = () => {
+    this.toggle(`dashboard-card-details-${this.issue.Id}`);
+  };
+
+  ngOnInit(): void {
+    const { issue } = this;
+    this.project = issue.Project ? issue.Project.Name || 'N/A' : 'N/A';
+    this.author = issue.Author ? getFullName(issue.Author) : 'N/A';
+    this.assignedTo = issue.AssignedTo ? getFullName(issue.AssignedTo) : 'N/A';
+  }
+}
